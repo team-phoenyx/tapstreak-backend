ed484ba7aef9655454e8e32b6e239ea14208a5e1
Avi <avi.glozman@gmail.com>
2017-05-20 14:53:17 -0700

    initial commit version 0.1.5202017 CODEDAY

---
+++ tapstreak-backend/tapstreak-backend/server.js
@@ -0,0 +1,21 @@
+'use strict';
+var express = require('express'),
+    app = express(),
+    port = process.env.PORT || 3000,
+    mongoose = require('mongoose'),
+    Task = require('./api/models/todoListModel'),
+    bodyParser = require('body-parser');
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://phoenyx-tapstreak:154vbqmXwtlRyxFKFCug7QrQGh57LycZdtjgkHuzJDpO4UOgGlKGD04EHMdYszEJwqffIitmXxqX8oPNGiXK0g==@phoenyx-tapstreak.documents.azure.com:10255/?ssl=true&replicaSet=globaldb');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var routes = require('./api/routes/todoListRoutes');
+routes(app);
+
+app.listen(port);
+var tag = 'CODEDAY';
+console.log('tapstreak API running on port' + port);
+console.log('version 0.1.5202017 - VERSION TAG: ' + tag);
