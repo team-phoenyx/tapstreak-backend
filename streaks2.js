54698f51f97058f256bde9cd9d7d03566acdae44
Terrance Li <terranceli.terbear@gmail.com>
2017-12-01 20:39:15 -0800

    fuck me

--- streaks2.js
+++ streaks2.js
@@ -2,13 +2,29 @@ var conn = new Mongo();
 var db = conn.getDB("tapstreakDP");
 
 while (true) {
-  print("iterate\n\n\n\n\n\n");
+  print("iterate");
 
   var cursor = db.users.find();
 
   while (cursor.hasNext()) {
-    var user = cursor.next();
-    printjson(user);
+    var thisUser = cursor.next();
+    var userStreaks = thisUser.streaks;
+    for (var j = 0; j < userStreaks.length; j++) {
+      if (Date.now() - userStreaks[j].last_streak > 30000) { //remove streak
+        print("remove streak");
+        var friend = db.users.findOne({_id: userStreaks[j].user_id}).next();
+        for (var k = 0; k < friend.streaks.length; k++) {
+          if (friend.streaks[k].user_id == thisUser.id) {
+            friend.streaks.splice(k, 1);
+            friend.save();
+            break;
+          }
+        }
+        userStreaks.splice(j, 1);
+        thisUser.streaks = userStreaks;
+        thisUser.save();
+      }
+    }
   }
 }
 /*
